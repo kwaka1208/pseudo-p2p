8652ba512c1c2cdc79ab0d5cb622c8fcf72e76df
Wakabayashi, Kenichi <kwaka1208@crssrds.jp>
2021-11-02 22:11:41 +0900

    モード切り替え操作の追加とモード表示タイミングの追加

--- main.ts
+++ main.ts
@@ -10,6 +10,10 @@ input.onButtonPressed(Button.A, function () {
         radio.sendValue(control.deviceName(), data)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    mode = (mode + 1) % 2
+    show_mode()
+})
 input.onButtonPressed(Button.B, function () {
     data += 1
     if (data > 9) {
@@ -24,13 +28,24 @@ radio.onReceivedValue(function (name, value) {
         // 送信モードなら、応答受信の処理を行う。
         if (control.deviceName() == name) {
             basic.showNumber(value)
+            basic.pause(1000)
+            show_mode()
         }
     } else {
-        radio.sendValue(name, value + 1)
+        _return = value + 1
+        if (_return > 9) {
+            _return = 0
+        }
+        radio.sendValue(name, _return)
     }
+    basic.showNumber(_return)
+    basic.pause(1000)
+    show_mode()
 })
+let _return = 0
 let data = 0
 let mode = 0
 mode = 0
 data = 1
 radio.setGroup(1)
+show_mode()
